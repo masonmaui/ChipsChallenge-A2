070231e0f67b8a1de0ee857e40d34ec0f8afb70a
danambz <danabuqrais@gmail.com>
2023-11-23 14:21:33 +0000

    Actor class

---
+++ Group16-A2/src/main/java/com/Actor.java
@@ -0,0 +1,37 @@
+public class Actor {
+
+    private int[] position;
+    private String name;
+
+    // Constructor
+    public Actor() {
+        // Default constructor
+    }
+
+    // Parameterized constructor
+    public Actor(int[] position, String name) {
+        this.position = position;
+        this.name = name;
+    }
+
+    // Method to create save data for the actor
+    public String createSaveData() {
+        // Assuming a simple format for save data
+        return "Position: " + position[0] + ", " + position[1] + "\nName: " + name;
+    }
+
+    // Getter method for position
+    public int[] getPosition() {
+        return position;
+    }
+
+    // Overridden toString() method for better representation
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "position=" + Arrays.toString(position) +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    
+}
