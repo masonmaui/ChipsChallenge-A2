eb03a437d0c6265327841d9dfc79665cc97382a4
Mason Emery <masonemery19@gmail.com>
2023-12-03 17:48:26 +0000

    Finished file reader for tiles

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/ChipSocket.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/ChipSocket.java
@@ -3,14 +3,16 @@ package com.example.group16a2.Tiles;
 public class ChipSocket extends Tile{
 
         private String filename;
+        private int chipsRequired;
 
-        public ChipSocket(boolean isInteractable) {
+        public ChipSocket(boolean isInteractable, int chipsRequired) {
             super(isInteractable);
             filename = "chipsocket.png";
+            this.chipsRequired = chipsRequired;
         }
 
         //to string
         public String toString() {
-            return "ChipSocket";
+            return "ChipSocket" + chipsRequired;
         }
 }
--- Group16-A2/src/main/java/com/example/group16a2/Tiles/TileLayer.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/TileLayer.java
@@ -43,10 +43,40 @@ public class TileLayer {
                     grid[lineCount][i] = new Path(false);
                 } else if (tileType.equals("D")) {
                     grid[lineCount][i] = new Dirt(true);
-                } else if (tileType.equals("W")){
+                } else if (tileType.equals("#")){
                     grid[lineCount][i] = new Wall(true);
                 } else if (tileType.equals("E")){
                     grid[lineCount][i] = new Exit(true);
+                }else if (tileType.equals("B")){
+                    grid[lineCount][i] = new Button(true);
+                }else if(tileType.equals("T")) {
+                    grid[lineCount][i] = new Trap(true);
+                }else if(tileType.equals("W")){
+                    grid[lineCount][i] = new Water(true);
+                }else if(tileType.equals("CS1")){
+                    grid[lineCount][i] = new ChipSocket(true, 1);
+                }else if(tileType.equals("CS2")) {
+                    grid[lineCount][i] = new ChipSocket(true, 2);
+                }else if(tileType.equals("CS3")) {
+                    grid[lineCount][i] = new ChipSocket(true, 3);
+                }else if(tileType.equals("RD")) {
+                    grid[lineCount][i] = new LockedDoorRed(true);
+                }else if(tileType.equals("GD")) {
+                    grid[lineCount][i] = new LockedDoorGreen(true);
+                }else if(tileType.equals("YD")) {
+                    grid[lineCount][i] = new LockedDoorYellow(true);
+                }else if(tileType.equals("BD")) {
+                    grid[lineCount][i] = new LockedDoorBlue(true);
+                }else if(tileType.equals("I")) {
+                    grid[lineCount][i] = new Ice(true);
+                }else if(tileType.equals("ITL")) {
+                    grid[lineCount][i] = new IceTopLeft(true);
+                }else if(tileType.equals("ITR")) {
+                    grid[lineCount][i] = new IceTopRight(true);
+                }else if(tileType.equals("IBL")) {
+                    grid[lineCount][i] = new IceBottomLeft(true);
+                }else if(tileType.equals("IBR")) {
+                    grid[lineCount][i] = new IceBottomRight(true);
                 }
             }
             lineCount++;
