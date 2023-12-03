c9e1f6162d96af96fcd9115dd4d189ef3f138222
MarcOhPolo <147829471+MarcOhPolo@users.noreply.github.com>
2023-12-03 23:15:10 +0000

    Added basics for collisions and key
    Mostly just outlined until all components necessary are added to the other layers

--- Group16-A2/src/main/java/com/example/group16a2/Game.java
+++ Group16-A2/src/main/java/com/example/group16a2/Game.java
@@ -1,4 +1,36 @@
 package com.example.group16a2;
 
+import com.example.group16a2.Tiles.TileLayer;
+
 public class Game {
+    //When a new game controller is made it will need to read the corrosponding layers' instances.
+    private TileLayer   TileLayer;
+    private ItemLayer   ItemLayer;
+    private Actor       ActorLayer;
+    private int currentlevel;
+
+    //When actor layer is made replace actor with ActorLayer
+    public Game(TileLayer TileLayer, ItemLayer ItemLayer, Actor ActorLayer, int currentlevel){
+        this.TileLayer  =   TileLayer;
+        this.ItemLayer  =   ItemLayer;
+        this.ActorLayer =   ActorLayer;
+        this.currentlevel = currentlevel;
+    }
+
+    public void CheckForAnyCollisions(){
+        if (this.checkBugCollisions()){
+            this.GameOver();
+        }
+    }
+
+    public Boolean checkBugCollisions(){
+        //for each bug in actor class
+        //if bug.getGeposition == player.getPosition then true, else:
+        return false;
+    }
+
+    public void GameOver(){
+        //This will interact with menu to display the new game.
+        //new Game(load next tiles, this.currentLevel+1)
+    }
 }
--- Group16-A2/src/main/java/com/example/group16a2/Key.java
+++ Group16-A2/src/main/java/com/example/group16a2/Key.java
@@ -1,5 +1,20 @@
 package com.example.group16a2;
 
 public class Key extends CollectableItems {
+    //type of door it accesses denoted by colour
+    private Character type;
 
+    public Key (Character type){
+        this.type = type;
+    }
+
+    public Character getType() {
+        return type;
+    }
+
+    public void Action(ItemLayer targetItemLayer){
+        //Add function for door to open.
+        //Need to complete functions to remove the chip from item layer once transaction is complete.
+        targetItemLayer.removeItemFromLayer(targetItemLayer.findItemInLayer(this),this);
+    }
 }
