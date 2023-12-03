dbf5c4b84f1801ac093201c8ec1cd65160ccac43
AmanB44 <147829390+AmanB44@users.noreply.github.com>
2023-12-03 23:40:47 +0000

    Create Frog.java

---
+++ Group16-A2/src/main/java/com/example/group16a2/Frog.java
@@ -0,0 +1,13 @@
+package com.example.group16a2;
+
+public class Frog extends Independent {
+
+    private String filename;
+
+    public Frog(String frog, int[] position, String name) {
+        super(position, name);
+
+        filename = "frog.png";
+
+    }
+}
