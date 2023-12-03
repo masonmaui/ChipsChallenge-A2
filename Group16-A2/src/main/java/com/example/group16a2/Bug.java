0694a368d1f7c1801a8bdcbd041e2f113b25a0ae
AmanB44 <147829390+AmanB44@users.noreply.github.com>
2023-12-03 23:40:23 +0000

    Create Bug.java

---
+++ Group16-A2/src/main/java/com/example/group16a2/Bug.java
@@ -0,0 +1,18 @@
+package com.example.group16a2;
+
+public class Bug extends Independent {
+
+    private String filename;
+
+    public Bug(String block, int[] position, String name) {
+        super(position, name);
+
+        filename = "bug.png";
+
+
+    }
+
+    public int[] bestPath(){
+        return null;
+    }
+}
