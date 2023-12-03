c6f2221b21e63966a748755fa4ac54aab199cf49
AmanB44 <147829390+AmanB44@users.noreply.github.com>
2023-12-03 23:43:01 +0000

    Create Block.java

---
+++ Group16-A2/src/main/java/com/example/group16a2/Block.java
@@ -0,0 +1,15 @@
+package com.example.group16a2;
+
+public class Block extends Independent {
+    private String block;
+
+    public Block(String block, int[] position, String name) {
+        super(position, name);
+        this.block = block;
+
+    }
+
+    public void push(){
+
+    }
+}
