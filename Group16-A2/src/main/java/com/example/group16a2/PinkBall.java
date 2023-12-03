8ff2774b9074db542a34051a56b1ee4b99bf710d
AmanB44 <147829390+AmanB44@users.noreply.github.com>
2023-12-03 23:38:49 +0000

    Create PinkBall.java

---
+++ Group16-A2/src/main/java/com/example/group16a2/PinkBall.java
@@ -0,0 +1,19 @@
+package com.example.group16a2;
+
+public class PinkBall extends Independent{
+    private String pinkBall;
+    private int[] inv;
+    private String filename;
+
+
+    public PinkBall(String pinkBall, int[] position, String name) {
+        super(position, name);
+        this.pinkBall = pinkBall;
+
+        filename = "pinkBall.png";
+    }
+
+    public void bounce(){
+
+    }
+}
