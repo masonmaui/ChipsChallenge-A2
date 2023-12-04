cc5ad6e69222d5c8ef6fa7a1a442894eaf7e1869
Aman Bhavra <147829390+AmanB44@users.noreply.github.com>
2023-12-04 18:03:05 +0000

    Updated the Bounce method

--- Group16-A2/src/main/java/com/example/group16a2/PinkBall.java
+++ Group16-A2/src/main/java/com/example/group16a2/PinkBall.java
@@ -12,7 +12,27 @@ public class PinkBall extends Independent{
         filename = "pinkBall.png";
     }
 
-    public void bounce(){
+    public void bounce() {
+        switch (direction) {
+            case "Right":
+                direction = "Left";
+                break;
 
+            case "Left":
+                direction = "Right";
+                break;
+
+            case "Up":
+                direction = "Down";
+                break;
+
+            case "Down":
+                direction = "Up";
+                break;
+
+            default:
+                break;
+
+        }
     }
 }
