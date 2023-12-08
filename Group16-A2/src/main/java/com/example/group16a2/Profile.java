9ffceb1d7c04e375e4daac6a5f63f2352c25650b
yushianluk <yushianluk@gmail.com>
2023-12-08 09:51:06 +0000

    updated the profile class

--- Group16-A2/src/main/java/com/example/group16a2/Profile.java
+++ Group16-A2/src/main/java/com/example/group16a2/Profile.java
@@ -1,8 +1,51 @@
 package com.example.group16a2;
 
 public class Profile extends Menu {
-
     private String name;
+    private int maxLevel;
 
-    private int nextLevel;
+    public void Profile(String name, int maxLevel) {
+        this.name=name;
+        this.maxLevel = maxLevel;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for maxLevel
+    public int getmaxLevel() {
+        return maxLevel;
+    }
+
+    // Setter for maxLevel
+    public void setMaxLevel(int maxLevel) {
+        this.maxLevel = maxLevel;
+    }
+
+
+    // Example method to display profile details
+    public void displayProfileDetails() {
+        System.out.println("Profile Details:");
+        System.out.println("Max Level: " + maxLevel);
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "name='" + name + '\'' +
+                ", maxLevel=" + maxLevel +
+                '}';
+    }
+
 }
+
+
+
+
