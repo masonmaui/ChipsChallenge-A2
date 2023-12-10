9db3d326f97c090dabf7099c05e14927d1286525
IsaacA27 <147829537+IsaacA27@users.noreply.github.com>
2023-12-10 21:43:42 +0000

    dirt and exit classes javadoc

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/Dirt.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/Dirt.java
@@ -1,19 +1,37 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * Dirt.java
+ * Represents a dirt tile
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class Dirt extends Tile {
 
     private String filename;
 
-    public Dirt(boolean isPassaable) {
-        super(isPassaable, "file:Sprites/Dirt.png");
+    /**
+     * Class constructor takes is passable as parameter
+     * @param isPassable represents whether tile can be moved past
+     */
+    public Dirt(boolean isPassable) {
+        super(isPassable, "file:Sprites/Dirt.png");
         filename = "file:Sprites/Dirt.png";
     }
 
+    /**
+     * get filename of image
+     * @return filename of image
+     */
     //get file name
     public String getFileName() {
         return filename;
     }
 
+    /**
+     * Returns a string with name of class
+     * @return name of class as string
+     */
     //to string
     public String toString() {
         return "Dirt";
--- Group16-A2/src/main/java/com/example/group16a2/Tiles/Exit.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/Exit.java
@@ -1,19 +1,37 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * Exit.java
+ * Represents an exit tile
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class Exit extends Tile {
 
     private String filename;
 
+    /**
+     * Class constructor takes isPassable as parameter
+     * @param isPassable represents whether tile can be moved past
+     */
     public Exit(boolean isPassable) {
         super(isPassable, "file:Sprites/Exit.png");
         filename = "file:Sprites/Exit.png";
     }
 
+    /**
+     * get filename of image
+     * @return filename of image
+     */
     //get file name
     public String getFileName() {
         return filename;
     }
 
+    /**
+     * Returns a string with name of class
+     * @return name of class as string
+     */
     //to string
     public String toString() {
         return "Exit";
