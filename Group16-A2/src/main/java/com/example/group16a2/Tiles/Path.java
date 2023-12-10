71f544c987dd67e7bf2636ad07664d9efbac6c5a
yushianluk <yushianluk@gmail.com>
2023-12-10 21:29:02 +0000

    add the javadoc for path class

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/Path.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/Path.java
@@ -1,8 +1,16 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * Path.java
+ * This class represents a tile that is part
+ * of a path in the game.
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class Path extends Tile {
 
     private String filename;
+
 
     public Path(boolean isPassable) {
         super(isPassable, "file:Sprites/Path.png");
