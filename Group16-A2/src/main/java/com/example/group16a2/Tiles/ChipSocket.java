7a42e4c1b7753e188b6dff961cf1f53bc8794c3f
Jimellis8596 <sc21jb2@leeds.ac.uk>
2023-12-10 19:01:24 +0000

    Chip socket changes
    code added to change the image for the number of chips required

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/ChipSocket.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/ChipSocket.java
@@ -6,7 +6,18 @@ public class ChipSocket extends Tile {
 
     public ChipSocket(boolean isPassable, int chipsRequired) {
         super(isPassable, "file:Sprites/ChipSocket.png");
-        filename = "file:Sprites/ChipSocket2.png";
+
+        // Corrected logic to set the filename based on the chipsRequired value
+        if (chipsRequired == 1) {
+            filename = "file:Sprites/ChipSocket1.png";
+        } else if (chipsRequired == 2) {
+            filename = "file:Sprites/ChipSocket2.png";  // Corrected filename
+        } else if (chipsRequired == 3) {
+            filename = "file:Sprites/ChipSocket3.png";
+        } else {
+            throw new IllegalArgumentException("Invalid chipsRequired value");
+        }
+
         this.chipsRequired = chipsRequired;
     }
 
