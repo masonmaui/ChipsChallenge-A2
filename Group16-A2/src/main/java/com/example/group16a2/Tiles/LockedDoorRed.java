da45ce1c500e321b7e2b4e136e2c739fe44ecbb2
yushianluk <yushianluk@gmail.com>
2023-12-10 21:16:59 +0000

    added javadoc to locked door red

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/LockedDoorRed.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/LockedDoorRed.java
@@ -1,13 +1,26 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * LockedDoorRed.java
+ * This class represents a tile that represents a red
+ * locked door.
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class LockedDoorRed extends Tile{
 
     private String filename;
 
+    /**
+     * Creates a new instance of the class, specifying whether
+     * the tile is passable or not.
+     * @param isPassable if the tile is passable, false otherwise.
+     */
     public LockedDoorRed(boolean isPassable) {
         super(isPassable, "file:Sprites/RedLock.png");
         filename = "file:Sprites/RedLock.png";
     }
+
 
     //get file name
     public String getFileName() {
