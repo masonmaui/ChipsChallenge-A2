a6cb71df06ae6fe5fd5ba9adfa78d821fd123003
IsaacA27 <147829537+IsaacA27@users.noreply.github.com>
2023-12-10 21:54:50 +0000

    Ice javadoc

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/Ice.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/Ice.java
@@ -1,21 +1,39 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * Ice.java
+ * Represents an ice tile
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class Ice extends Tile{
 
         private String filename;
 
-        public Ice(boolean isPassable) {
+    /**
+     * Class constructor takes isPassable as parameter
+     * @param isPassable represents whether tile can be moved past
+     */
+    public Ice(boolean isPassable) {
             super(isPassable, "file:Sprites/ice.png");
             filename = "file:Sprites/ice.png";
         }
 
+    /**
+     * get filename of image
+     * @return filename of image
+     */
         //get file name
-        public String getFileName() {
-            return filename;
-        }
+    public String getFileName() {
+        return filename;
+    }
 
+    /**
+     * Returns a string with name of class
+     * @return name of class as string
+     */
         //to string
-        public String toString() {
-            return "Ice";
-        }
+    public String toString() {
+        return "Ice";
+    }
 }
--- Group16-A2/src/main/java/com/example/group16a2/Tiles/IceBottomLeft.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/IceBottomLeft.java
@@ -1,19 +1,37 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * IceBottomLeft.java
+ * Represents an ice tile that has a corner in the bottom left
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class IceBottomLeft extends Tile{
 
             private String filename;
 
+            /**
+             * Class constructor takes isPassable as parameter
+             * @param isPassable represents whether tile can be moved past
+            */
             public IceBottomLeft(boolean isPassable) {
                 super(isPassable, "file:Sprites/IceBottomLeft.png");
                 filename = "file:Sprites/IceBottomLeft.png";
             }
 
+            /**
+            * get filename of image
+            * @return filename of image
+            */
             //get file name
             public String getFileName() {
                 return filename;
             }
 
+            /**
+             * Returns a string with name of class
+             * @return name of class as string
+            */
             //to string
             public String toString() {
                 return "IceBottomLeft";
