9550b8e55a3f516fc8200ea7abe66fef53443e31
IsaacA27 <147829537+IsaacA27@users.noreply.github.com>
2023-12-10 22:15:32 +0000

    last of ice javadoc

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/IceTopLeft.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/IceTopLeft.java
@@ -1,19 +1,37 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * IceTopLeft.java
+ * Represents an ice tile that has a corner in the top left
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class IceTopLeft extends Tile{
 
             private String filename;
 
+            /**
+            * Class constructor takes isPassable as parameter
+            * @param isPassable represents whether tile can be moved past
+            */
             public IceTopLeft(boolean isPassable) {
                 super(isPassable, "file:Sprites/IceTopLeft.png");
                 filename = "file:Sprites/IceTopLeft.png";
             }
 
+            /**
+            * get filename of image
+            * @return filename of image
+            */
             //get file name
             public String getFileName() {
                 return filename;
             }
 
+            /**
+            * Returns a string with name of class
+            * @return name of class as string
+            */
             //to string
             public String toString() {
                 return "IceTopLeft";
