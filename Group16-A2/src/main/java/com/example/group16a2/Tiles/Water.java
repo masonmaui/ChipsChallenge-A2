b197ef63eab2e5d09d7bd765ecc7010af7936088
yushianluk <yushianluk@gmail.com>
2023-12-10 21:39:18 +0000

    add java doc on water

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/Water.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/Water.java
@@ -1,5 +1,12 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * Water.java
+ * This class represents the tile that is associated with
+ * water in the game.
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class Water extends Tile{
 
     private String filename;
