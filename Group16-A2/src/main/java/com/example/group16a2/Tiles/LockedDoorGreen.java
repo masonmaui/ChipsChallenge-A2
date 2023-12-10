70719491d7654397905b490f78280e907d745e7f
yushianluk <yushianluk@gmail.com>
2023-12-10 21:05:20 +0000

    added javadoc on locked door green

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/LockedDoorGreen.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/LockedDoorGreen.java
@@ -1,5 +1,12 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * LockedDoorGreen.java
+ * This class represents a tile that represents a green
+ * locked door.
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class LockedDoorGreen extends Tile{
 
     private String filename;
