7f7c4681cc276a6013276264f8496cf28945613c
yushianluk <yushianluk@gmail.com>
2023-12-10 21:45:35 +0000

    add javadoc on trap class

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/Trap.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/Trap.java
@@ -1,5 +1,12 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * Trap.java
+ * This class represents a trap which is part of the tile class
+ * that can be used in the game.
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class Trap extends Tile {
 
     private String filename;
