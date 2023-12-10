46c1f4dc79528764d0433282fe695e9932fbb1e3
IsaacA27 <147829537+IsaacA27@users.noreply.github.com>
2023-12-10 22:03:01 +0000

    ice and blue door javadoc

--- Group16-A2/src/main/java/com/example/group16a2/Tiles/IceBottomRight.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/IceBottomRight.java
@@ -1,21 +1,38 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * IceBottomRight.java
+ * Represents an ice tile that has a corner in the bottom right
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class IceBottomRight extends Tile{
 
-            private String filename;
+    private String filename;
 
-            public IceBottomRight(boolean isPassable) {
-                super(isPassable, "file:Sprites/IceBottomRight.png");
-                filename = "file:Sprites/IceBottomRight.png";
-            }
+    /**
+     * Class constructor takes isPassable as parameter
+     * @param isPassable represents whether tile can be moved past
+     */
+    public IceBottomRight(boolean isPassable) {
+        super(isPassable, "file:Sprites/IceBottomRight.png");
+        filename = "file:Sprites/IceBottomRight.png";
+    }
 
-            //get file name
-            public String getFileName() {
+    /**
+     * get filename of image
+     * @return filename of image
+     */
+    //get file name
+    public String getFileName() {
                 return filename;
             }
 
-            //to string
-            public String toString() {
-                return "IceBottomRight";
-            }
+    /**
+     * Returns a string with name of class
+     * @return name of class as string
+     */
+    public String toString(){
+        return "IceBottomRight";
+    }
 }
--- Group16-A2/src/main/java/com/example/group16a2/Tiles/LockedDoorBlue.java
+++ Group16-A2/src/main/java/com/example/group16a2/Tiles/LockedDoorBlue.java
@@ -1,19 +1,37 @@
 package com.example.group16a2.Tiles;
 
+/**
+ * LockedDoorBlue.java
+ * Represents a blue locked door tile
+ * @author Mason Emery
+ * @version 1.6
+ */
 public class LockedDoorBlue extends Tile{
 
     private String filename;
 
+    /**
+     * Class constructor takes isPassable as parameter
+     * @param isPassable represents whether tile can be moved past
+     */
     public LockedDoorBlue(boolean isPassable) {
         super(isPassable, "file:Sprites/BlueLock.png");
         filename = "file:Sprites/BlueLock.png";
     }
 
+    /**
+     * get filename of image
+     * @return filename of image
+     */
     //get file name
     public String getFileName() {
         return filename;
     }
 
+    /**
+     * Returns a string with name of class
+     * @return name of class as string
+     */
     //to string
     public String toString() {
         return "LockedDoorBlue";
