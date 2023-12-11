4a515a9a6d910e4c9c6e0f0f148a4b7b5bc1e918
IsaacA27 <147829537+IsaacA27@users.noreply.github.com>
2023-12-11 00:30:12 +0000

    add javadoc for bug

--- Group16-A2/src/main/java/com/example/group16a2/Actors/Bug.java
+++ Group16-A2/src/main/java/com/example/group16a2/Actors/Bug.java
@@ -17,6 +17,12 @@ public class Bug extends Actor{
 
     private String wallToFollow;
 
+    /**
+     * class constructor
+     * @param y represents y co-ordinate of bug
+     * @param x represents x co-ordinate of bug
+     * @param wallToFollow represents whether bug will follow left edge or right edge
+     */
     public Bug(int y, int x, String wallToFollow){
         super(y, x, "file:Sprites/BugUp.png");
         this.fileName = "file:Sprites/BugUp.png";
@@ -25,21 +31,37 @@ public class Bug extends Actor{
         this.wallToFollow = wallToFollow;
     }
 
+    /**
+     * return filename of image
+     * @return image filename
+     */
     //get file name
     public String getFileName(){
         return fileName;
     }
 
+    /**
+     * return x co-ordinate of bug
+     * @return x co-ordiante
+     */
     //get x
     public int getX(){
         return x;
     }
 
+    /**
+     * return y co-ordinate of bug
+     * @return y co-ordinate
+     */
     //get y
     public int getY(){
         return y;
     }
 
+    /**
+     * determines which edge bug is following and moves bug accordingly
+     * @param tile represents the grid of tiles
+     */
     //wall follow using left and right
     public void wallFollow(Tile[][] tile){
         if (wallToFollow.equals("L")) {
@@ -98,6 +120,11 @@ public class Bug extends Actor{
         }
     }
 
+    /**
+     * moves bug to the right one tile
+     * first checks whether bug can move to desired tile
+     * @param tile represents a grid of tiles
+     */
     //move right
     public void moveRight(Tile[][] tile){
         if (isInBounds(x + 1, y, tile)){
@@ -107,6 +134,11 @@ public class Bug extends Actor{
         }
     }
 
+    /**
+     * moves bug to the left one tile
+     * first checks whether bug can move to desired tile
+     * @param tile represents a grid of tiles
+     */
     //move left
     public void moveLeft(Tile[][] tile){
         if (isInBounds(x - 1, y, tile)){
@@ -116,6 +148,11 @@ public class Bug extends Actor{
         }
     }
 
+    /**
+     * moves bug up one tile
+     * first checks whether bug can move to desired tile
+     * @param tile represents the grid of tiles
+     */
     //move up
     public void moveUp(Tile[][] tile){
         if (isInBounds(x, y - 1, tile)){
@@ -125,6 +162,11 @@ public class Bug extends Actor{
         }
     }
 
+    /**
+     * moves bug down one tile
+     * first checks whether bug can move to desired tile
+     * @param tile represents a grid of tiles
+     */
     //move down
     public void moveDown(Tile[][] tile){
         if (isInBounds(x, y + 1, tile)){
@@ -133,6 +175,14 @@ public class Bug extends Actor{
             }
         }
     }
+
+    /**
+     * checks if the tile to be moved to is in bounds
+     * @param x represents the x co-ordinate of the bug
+     * @param y represents the x co-ordinate of the bug
+     * @param tile represents the grid of tiles
+     * @return true if tile to be moved to is in bounds, false otherwise
+     */
     public boolean isInBounds(int x, int y, Tile[][] tile){
         if (x < 0 || x >= tile[0].length || y < 0 || y >= tile.length){
             return false;
@@ -140,6 +190,10 @@ public class Bug extends Actor{
         return true;
     }
 
+    /**
+     * returns a string with the name of the class
+     * @return name of the class as a string
+     */
     //to string
     public String toString(){
         return "Bug";
